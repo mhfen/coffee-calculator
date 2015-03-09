acbfd24998429fc536d2018f255ea5ab7c01d9b9
Matt Fender <mhfender@gmail.com>
2015-03-08 21:10:36 -0500

    Form styling. Form validation. Strength options. FOIT fix.

--- src/assets/js/controllers.js
+++ src/assets/js/controllers.js
@@ -4,14 +4,14 @@
 angular.module('coffeeApp.controllers', [])
 	.controller('mainController', function($scope, $location, CoffeeService) {
 
-		$scope.calculateCoffee = function(coffeeForm, strength) {
-			CoffeeService.setStrength(strength);
+		$scope.calculateCoffee = function(coffeeForm) {
+			CoffeeService.setStrength(coffeeForm.strength);
 			$location.path('/results/' + coffeeForm.cups);
 		};
 
 	})
 	.controller('resultsController', function($scope, $routeParams, CoffeeService) {
-		$scope.strength = CoffeeService.getStrength();
+		$scope.strength = CoffeeService.getStrength() || 17.42;
 		$scope.utils = coffeeApp.utils;
 		$scope.cups = $routeParams.cups;
 		$scope.waterVolume = $scope.utils.calculateWater($routeParams.cups);
